95a62b18967386635a27354ecce483773e3343a8
Hack Reactor Student <student@hackreactor.com>
2017-09-01 16:19:09 -0700

    Passing all tests up to add friend

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,104 +1,170 @@
 // YOUR CODE HERE:
-$(document).ready(() => {
+class App {
+  constructor() {
+    this.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
+    this.friends = new Set();
+  }
 
-	window.rooms = [];
-	window.messages = [];
+  init() {
+  
+  }
 
-	window.currentRoom = 'lobby';
+  send(message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: this.server,
+      type: 'POST',
+      data: message,//JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  }
 
-	var autoRefresh = () => {
-		setTimeout(() => { autoRefresh() }, 10000);
+  fetch() {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: this.server,
+      type: 'GET',
+      success: function (data) {
+        console.log('chatterbox: Message get');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to get message', data);
+      }
+    });
+  }
 
-		$.ajax({
-		  // This is the url you should use to communicate with the parse API server.
-		  url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-		  type: 'GET',
-		  success: function (data) {
-		    console.log('chatterbox: Message get');
-		    window.messages = data.results;
-		    handleData();
-		  },
-		  error: function (data) {
-		    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-		    console.error('chatterbox: Failed to get message', data);
-		  }
-		});
+  clearMessages() {
+    $('#chats').empty();
+  }
 
-	}
-	autoRefresh()
+  renderMessage(message) {
+    var $message = $(`<div class="message"></div>`);
+    var $username = $(`<button class="username">${message.username}</button>`);
+    var $messageContent = $(`<h3>${message.text}</h3>`);
+    $message.append($username, $messageContent);
+    $('#chats').append($message);
+  }
 
-	var handleData = () => {
-		handleRooms();
-		handleMessages();
-	}
+  renderRoom() {
+    var $option = $(`<option>${name}</option>`);
+    $('#roomSelect').append($option);
+  }
+  
+  handleUsernameClick(name) {
+    if (name !== this.username) {
+      this.friends.add(name);
+      return true;
+    }
+    return false;
+  }
 
-	var handleRooms = () => {
-		var roomnames = [];
-		_.each(messages, (message) => {
-			roomnames.push(message.roomname);
-		});
-		window.roomnames = _.uniq(roomnames);
-		$('.roomList').empty();
-		$('.roomList').append(`<option>${window.currentRoom}</option>`);
-		_.each(window.roomnames, (name) => {
-			var $option = $(`<option>${name}</option>`);
-			$('.roomList').append($option);
-		});
-	}
+  handleSubmit() {
+    console.log("HANDLE SUBMIT");
+    return true;
+  }
+}
 
-	var handleMessages = () => {
-		var roomMessages = _.filter(window.messages, (message) => {
-			return message.roomname === window.currentRoom;
-		});
-		$('#chats').empty();
-		_.each(roomMessages, (message) => {
-			var $message = $(`<h3>${message.username}: ${message.text}</h3>`);
-			$('#chats').append($message);
-		});
-	}
+var app = new App();
 
-	var message = {
-	  username: 'test',
-	  text: 'trololo',
-	  roomname: '4chan'
-	};
+$(document).on('click', '.username', function(event) {
+  app.handleUsernameClick();
+});
 
-	$('.roomList').change(function (event) {
-		window.currentRoom = $(this).val();
+// $(document).on('click', '.username', function(event) {
+//   app.handleUsernameClick();
+// });
+
+
+$(document).ready(function() {
+
+  var app = new App();
+
+  window.rooms = [];
+  window.messages = [];
+
+  window.currentRoom = 'lobby';
+
+  var autoRefresh = () => {
+    setTimeout(() => { autoRefresh(); }, 10000);
+
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+      type: 'GET',
+      success: function (data) {
+        console.log('chatterbox: Message get');
+        window.messages = data.results;
+        handleData();
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to get message', data);
+      }
+    });
+
+  };
+
+  autoRefresh();
+
+  var handleData = () => {
+    handleRooms();
+    handleMessages();
+  };
+
+  var handleRooms = () => {
+    var roomnames = [];
+    _.each(messages, (message) => {
+      roomnames.push(message.roomname);
+    });
+    window.roomnames = _.uniq(roomnames);
+    $('.roomList').empty();
+    $('.roomList').append(`<option>${window.currentRoom}</option>`);
+    _.each(window.roomnames, (name) => {
+      var $option = $(`<option>${name}</option>`);
+      $('.roomList').append($option);
+    });
+  };
+
+  var handleMessages = () => {
+    var roomMessages = _.filter(window.messages, (message) => {
+      return message.roomname === window.currentRoom;
+    });
+    $('#chats').empty();
+    _.each(roomMessages, (message) => {
+      app.renderMessage(message);
+    });
+  };
+
+  $('#roomSelect').change(function (event) {
+    window.currentRoom = $(this).val();
     handleMessages();
   });
 
-	$('.testSendButton').on('click', function(event) {
-		$.ajax({
-		  // This is the url you should use to communicate with the parse API server.
-		  url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-		  type: 'POST',
-		  data: JSON.stringify(message),
-		  contentType: 'application/json',
-		  success: function (data) {
-		    console.log('chatterbox: Message sent');
-		  },
-		  error: function (data) {
-		    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-		    console.error('chatterbox: Failed to send message', data);
-		  }
-		});
-  })
+  // $('.username').on('click', function(event) {
+  //  app.handleUsernameClick();
+  // });
 
-  $('.testGetButton').on('click', function(event) {
-		$.ajax({
-		  // This is the url you should use to communicate with the parse API server.
-		  url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-		  type: 'GET',
-		  success: function (data) {
-		    console.log('chatterbox: Message get');
-		    console.log(JSON.stringify(data));
-		  },
-		  error: function (data) {
-		    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-		    console.error('chatterbox: Failed to get message', data);
-		  }
-		});
-  })
+  // $('body').on('click', '.username', function(event) {
+  //  app.handleUsernameClick();
+  // });
 
-})
+  // $(this).on('click', '.username', function(event) {
+  //   app.handleUsernameClick();
+  // });
+
+  // $('.testSendButton').on('click', function(event) {
+    
+ //  });
+
+ //  $('.testGetButton').on('click', function(event) {
+    
+ //  });
+});

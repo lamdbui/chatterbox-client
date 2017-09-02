9ec04022c6065f9118bdd54d5d01430fa46f66a3
Hack Reactor Student <student@hackreactor.com>
2017-09-01 18:54:04 -0700

    Finish implementing send message functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,6 +3,9 @@ class App {
   constructor() {
     this.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
     this.friends = new Set();
+    this.rooms = [];
+    this.messages = [];
+    this.currentRoom = 'lobby';
   }
 
   init() {
@@ -10,14 +13,17 @@ class App {
   }
 
   send(message) {
+    var that = this;
+
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: this.server,
       type: 'POST',
-      data: message,//JSON.stringify(message),
+      data: message,
       contentType: 'application/json',
       success: function (data) {
         console.log('chatterbox: Message sent');
+        that.refresh();
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -47,7 +53,7 @@ class App {
 
   renderMessage(message) {
     var $message = $(`<div class="message"></div>`);
-    var $username = $(`<button class="username">${message.username}</button>`);
+    var $username = $(`<a class="username">${message.username}</a>`);
     var $messageContent = $(`<h3>${message.text}</h3>`);
     $message.append($username, $messageContent);
     $('#chats').append($message);
@@ -61,14 +67,72 @@ class App {
   handleUsernameClick(name) {
     if (name !== this.username) {
       this.friends.add(name);
-      return true;
     }
-    return false;
   }
 
-  handleSubmit() {
-    console.log("HANDLE SUBMIT");
-    return true;
+  handleSubmit(message) {
+    var messageObj = {
+      username: this.username,
+      text: message,
+      roomname: this.currentRoom
+    };
+    this.send(JSON.stringify(messageObj));
+  }
+
+  handleData() {
+    this.handleRooms();
+    this.handleMessages();
+  }
+
+  handleRooms() {
+    var roomnames = [];
+    _.each(this.messages, (message) => {
+      roomnames.push(message.roomname);
+    });
+    this.rooms = _.uniq(roomnames);
+    $('#roomSelect').empty();
+    $('#roomSelect').append(`<option>${this.currentRoom}</option>`);
+    _.each(this.rooms, (name) => {
+      if (name !== this.currentRoom) {
+        var $option = $(`<option>${name}</option>`);
+        $('#roomSelect').append($option);
+      }
+    });
+  }
+
+  handleMessages() {
+    var roomMessages = _.filter(this.messages, (message) => {
+      return message.roomname === this.currentRoom;
+    });
+    $('#chats').empty();
+    _.each(roomMessages, (message) => {
+      this.renderMessage(message);
+    });
+  }
+
+  autoRefresh() {
+    setTimeout(this.autoRefresh.bind(this), 10000);
+    
+    this.refresh();
+  }
+
+  refresh() {
+    var that = this;
+
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages?limit=200&order=-createdAt',
+      type: 'GET',
+      success: function (data) {
+        console.log('chatterbox: Message get');
+        that.messages = data.results;
+        that.handleData();
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to get message', data);
+      }
+    });
   }
 }
 
@@ -78,93 +142,20 @@ $(document).on('click', '.username', function(event) {
   app.handleUsernameClick();
 });
 
-// $(document).on('click', '.username', function(event) {
-//   app.handleUsernameClick();
-// });
+$(document).on('click', '.submit', function(event) {
+  app.handleSubmit($('#message').val());
+});
 
+$(document).on('change', '#roomSelect', function(event) {
+  app.currentRoom = $(this).val();
+  app.handleMessages();
+});
 
 $(document).ready(function() {
 
-  var app = new App();
+  var urlString = window.location.href;
+  var url = new URL(urlString);
+  app.username = url.searchParams.get('username');
+  app.autoRefresh();
 
-  window.rooms = [];
-  window.messages = [];
-
-  window.currentRoom = 'lobby';
-
-  var autoRefresh = () => {
-    setTimeout(() => { autoRefresh(); }, 10000);
-
-    $.ajax({
-      // This is the url you should use to communicate with the parse API server.
-      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-      type: 'GET',
-      success: function (data) {
-        console.log('chatterbox: Message get');
-        window.messages = data.results;
-        handleData();
-      },
-      error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to get message', data);
-      }
-    });
-
-  };
-
-  autoRefresh();
-
-  var handleData = () => {
-    handleRooms();
-    handleMessages();
-  };
-
-  var handleRooms = () => {
-    var roomnames = [];
-    _.each(messages, (message) => {
-      roomnames.push(message.roomname);
-    });
-    window.roomnames = _.uniq(roomnames);
-    $('.roomList').empty();
-    $('.roomList').append(`<option>${window.currentRoom}</option>`);
-    _.each(window.roomnames, (name) => {
-      var $option = $(`<option>${name}</option>`);
-      $('.roomList').append($option);
-    });
-  };
-
-  var handleMessages = () => {
-    var roomMessages = _.filter(window.messages, (message) => {
-      return message.roomname === window.currentRoom;
-    });
-    $('#chats').empty();
-    _.each(roomMessages, (message) => {
-      app.renderMessage(message);
-    });
-  };
-
-  $('#roomSelect').change(function (event) {
-    window.currentRoom = $(this).val();
-    handleMessages();
-  });
-
-  // $('.username').on('click', function(event) {
-  //  app.handleUsernameClick();
-  // });
-
-  // $('body').on('click', '.username', function(event) {
-  //  app.handleUsernameClick();
-  // });
-
-  // $(this).on('click', '.username', function(event) {
-  //   app.handleUsernameClick();
-  // });
-
-  // $('.testSendButton').on('click', function(event) {
-    
- //  });
-
- //  $('.testGetButton').on('click', function(event) {
-    
- //  });
 });
